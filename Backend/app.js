4a2e0520f0b0b89d0b290a5329edf165266ca6c0
asma-aslam30 <aslamasma986@gmail.com>
2025-04-16 12:34:38 +0500

    new

--- Backend/app.js
+++ Backend/app.js
@@ -7,20 +7,29 @@ const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 const captainRoutes = require('./routes/captain.routes');
+
+// Connect to database
 connectToDb();
 
+// 👇 CORS configuration with your deployed frontend URL
+const corsOptions = {
+    origin: 'https://uber-zfn2-git-master-asma-aslam30s-projects.vercel.app',
+    credentials: true
+};
 
+// Middleware setup
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Test route
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-
-app.use('/users', userRoutes)
-app.use('/captains', captainRoutes)
+// API routes
+app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
 
 module.exports = app;
